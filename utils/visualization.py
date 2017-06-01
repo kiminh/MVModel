860fd8f266c6d089118df10bdd3bbf55c0eeb430
mingyangShang <shangmingyanggo@gmail.com>
2017-06-01 15:18:35 +0800

    repeat display

--- utils/visualization.py
+++ utils/visualization.py
@@ -21,14 +21,14 @@ features_relu = np.load('../ignore/test_12p_vgg19_29epo_feature.npy')
 features_tanh = np.load('../ignore/test_12p_vgg19_epo29_tanh7_feature.npy')
 
 
-def show_model_features(index=0, type='tanh'):
+def show_model_features(index=0, type='tanh', normalize=True):
     # do transformation
     data_relu = features_relu[index]
     data_tanh = features_tanh[index]
 
     max_f_relu, min_f_relu = np.max(data_relu), np.min(data_relu)
     max_f_tanh, min_f_tanh = np.max(data_tanh), np.min(data_tanh)
-    if type == 'tanh' or type=='relu':
+    if normalize:
         for i in xrange(np.shape(data_relu)[0]):
             for j in xrange(np.shape(data_relu)[1]):
                 data_relu[i][j] = normalization(min_f_relu, max_f_relu, data_relu[i][j])
@@ -37,10 +37,12 @@ def show_model_features(index=0, type='tanh'):
             for j in xrange(np.shape(data_tanh)[1]):
                 data_tanh[i][j] = normalization(min_f_tanh, max_f_tanh, data_tanh[i][j])
         max_f_tanh, min_f_tanh = np.max(data_tanh), np.min(data_tanh)
-    for i in xrange(7):
-        data_relu = np.concatenate((data_relu, data_relu))
-    for i in xrange(7):
-        data_tanh = np.concatenate((data_tanh, data_tanh))
+    # for i in xrange(7):
+    #     data_relu = np.concatenate((data_relu, data_relu))
+    # for i in xrange(7):
+    #     data_tanh = np.concatenate((data_tanh, data_tanh))
+    data_relu = np.repeat(data_relu, 200, axis=0)
+    data_tanh = np.repeat(data_tanh, 200, axis=0)
 
     fig_relu, ax_relu = plt.subplots()
     fig_relu.suptitle("relu")
@@ -60,5 +62,5 @@ def normalization(min_v, max_v, v):
 # 4, 7, 12
 # 64,65,66
 if __name__ == '__main__':
-    show_model_features(index=66)
+    show_model_features(index=66, normalize=False)
     plt.show()

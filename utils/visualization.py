d323a8107c3c9b0d188267744c2f482e75e94923
mingyangShang <shang_ming_yang@163.com>
2017-07-03 15:58:02 +0800

    Revert "add attention difference visualization"
    This reverts commit 7e8c522f3c12c5c53a7cbc5b6e4690503f8a8603.

--- utils/visualization.py
+++ utils/visualization.py
@@ -109,25 +109,25 @@ def show_proj_w_dis(proj_file, distance=euclidean):
 def show_attention(attn_file, model_index=0):
     attns = np.load(attn_file)
     model_attn = attns[model_index]
-    max_value = np.max(model_attn)
-    print(np.sum(model_attn, axis=1))
-    for i in xrange(model_attn.shape[0]):
-        for j in xrange(model_attn.shape[1]):
-            model_attn[i][j] = model_attn[i][j] / max_value
-
+    print(model_attn)
     fig, ax = plt.subplots()
     fig.suptitle("%s-%d"%(attn_file[attn_file.rfind('/')+1:], model_index))
     cax = ax.imshow(model_attn, cmap="gray", interpolation="nearest")
     cbar = fig.colorbar(cax, ticks=[np.min(model_attn), np.max(model_attn)])
     cbar.ax.set_yticklabels([str(np.min(model_attn)), str(np.max(model_attn))])
 
-def show_attention_diff(attn_file, model_index1=0, model_index2=-1):
+def show_attention2(attn_file, model_index=0):
     attns = np.load(attn_file)
-    model_attn1, model_attn2 = attns[model_index1], attns[model_index2]
-    model_attn = model_attn1 - model_attn2 # difference
+    model_attn = attns[model_index]
+    classes, views = model_attn.shape[0], model_attn.shape[1]
+    for i in xrange(classes):
+        s = 0.0
+        for j in xrange(views):
+            s += (model_attn[i][j] * model_attn[i][j])
+        for j in xrange(views):
+            model_attn[i][j] = model_attn[i][j] * model_attn[i][j] / s
     fig, ax = plt.subplots()
-    print np.max(model_attn)
-    fig.suptitle("%s-%d VS %d" % (attn_file[attn_file.rfind('/') + 1:], model_index1, model_index2))
+    fig.suptitle("%s-%d" % (attn_file[attn_file.rfind('/') + 1:], model_index))
     cax = ax.imshow(model_attn, cmap="gray", interpolation="nearest")
     cbar = fig.colorbar(cax, ticks=[np.min(model_attn), np.max(model_attn)])
     cbar.ax.set_yticklabels([str(np.min(model_attn)), str(np.max(model_attn))])
@@ -144,6 +144,5 @@ if __name__ == '__main__':
     # show_proj_w_dis("../ignore/data/proj_w_attn.npy", distance=euclidean)
     # show_proj_w_dis(proj_file, distance=cosine)
     # show_proj_w_dis("../ignore/data/proj_w_attn.npy", distance=cosine)
-    # show_attention('../ignore/data/attention_weights_2048.npy', model_index=100)
-    show_attention_diff('../ignore/data/attention_weights_2048.npy', model_index1=0, model_index2=56)
+    show_attention('../ignore/data/attention_weights_1view.npy', model_index=715)
     plt.show()

328b2e0ed87462eb11f5b4291181764f1cafc47c
mingyangShang <shang_ming_yang@163.com>
2017-07-22 17:05:28 +0800

    add test2test and train2train

--- utils/retrival.py
+++ utils/retrival.py
@@ -11,6 +11,7 @@ def retrival_distance(test_feature_file, train_feature_file, savepath='test_trai
 def generate_distance_test2test(test_feature_file, savepath='test_test'):
     test_data = np.load(test_feature_file)
     result = [euclidean(test1, test2) for test1 in test_data for test2 in test_data]
+    result = np.reshape(np.array(result), [test_data.shape[0], test_data.shape[0]])
     np.save(savepath, result)
 
 def retrival_all_distance(test_feature_file, train_feature_file, savepath='all_all'):
@@ -52,8 +53,9 @@ def retrival_metrics_test2train(sims_file, test_labels_file, train_labels_file):
 
 if __name__ == '__main__':
     #retrival_distance('/home1/shangmingyang/data/3dmodel/mvmodel_result/retrival/test_hidden.npy', '/home1/shangmingyang/data/3dmodel/mvmodel_result/retrival/train_hidden.npy', '/home1/shangmingyang/data/3dmodel/mvmodel_result/retrival/test_train_euclidean')
-    generate_distance_test2test('/home1/shangmingyang/data/3dmodel/mvmodel_result/retrival/test_hidden.npy', '/home1/shangmingyang/data/3dmodel/mvmodel_result/retrival/test_test_euclidean')
+    generate_distance_test2test('/home1/shangmingyang/data/3dmodel/mvmodel_result/retrival/train_hidden.npy', '/home1/shangmingyang/data/3dmodel/mvmodel_result/retrival/train_train_euclidean')
     #retrival_all_distance('/home/shangmingyang/projects/MVModel/rnn/test_hidden.npy', '/home/shangmingyang/projects/MVModel/rnn/train_hidden.npy', 'all_all_euclidean')
     #generate_labels_all('/home3/lhl/modelnet40_total_v2/test_label.npy', '/home3/lhl/modelnet40_total_v2/train_label.npy', '/home1/shangmingyang/data/3dmodel/mvmodel_result/retrival/all_labels')
     #retrival_metrics_all('/home1/shangmingyang/data/3dmodel/mvmodel_result/retrival/all_all_euclidean.npy', '/home1/shangmingyang/data/3dmodel/mvmodel_result/retrival/all_labels.npy')
     #retrival_metrics_test2train('/home1/shangmingyang/data/3dmodel/mvmodel_result/retrival/test_train_euclidean.npy', '/home3/lhl/modelnet40_total_v2/test_label.npy', '/home3/lhl/modelnet40_total_v2/train_label.npy')
+    retrival_metrics_all('/home1/shangmingyang/data/3dmodel/mvmodel_result/retrival/train_train_euclidean.npy', '/home3/lhl/modelnet40_total_v2/train_label.npy')

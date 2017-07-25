1d99c079034d1bcc1bd5d7daec9a002379eccebf
mingyangShang <shang_ming_yang@163.com>
2017-07-25 13:42:33 +0800

    experiment

--- rnn/run_test.py
+++ rnn/run_test.py
@@ -1,5 +1,5 @@
 import os
-checkpoint_file = "/home1/shangmingyang/data/3dmodel/trained_seq_mvmodel/modelnet10_6views/checkpoint"
+checkpoint_file = "/home1/shangmingyang/data/3dmodel/trained_seq_mvmodel/modelnet10_3views/checkpoint"
 with open(checkpoint_file) as f:
     data = f.readlines()[1:]
     data = [line.split(":")[1] for line in data]
@@ -8,4 +8,4 @@ with open(checkpoint_file) as f:
 print data
 
 for model_path in data:
-    os.system('python train.py --train=False --n_views=6 --seq_embeddingmvmodel_path=%s --test_acc_file=%s --n_hidden=%d --decoder_embedding_size=%d --enrich_data=%s --use_lstm=False' %(model_path, '/home1/shangmingyang/data/3dmodel/mvmodel_result/modelnet10/modelnet10_6views.csv', 128, 256, "False"))
+    os.system('python train.py --train=False --n_views=3 --seq_embeddingmvmodel_path=%s --test_acc_file=%s --n_hidden=%d --decoder_embedding_size=%d --enrich_data=%s --use_lstm=False' %(model_path, '/home1/shangmingyang/data/3dmodel/mvmodel_result/modelnet10/modelnet10_3views.csv', 128, 256, "False"))
